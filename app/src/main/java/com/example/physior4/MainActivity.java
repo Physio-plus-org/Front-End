6f31ca243b11d242b1fa83ae473e47179862a2eb
NikosSakell <nikossakell02@gmail.com>
2023-05-08 17:27:22 +0300

    Front-End R4

---
+++ app/src/main/java/com/example/physior4/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.physior4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import android.os.Bundle;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).hide(); // Hide the action bar
+
+        /*SearchView searchView = findViewById(R.id.searchView);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Perform search operation here
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Filter search results here
+                return false;
+            }
+        });*/
+
+    }
+
+}
