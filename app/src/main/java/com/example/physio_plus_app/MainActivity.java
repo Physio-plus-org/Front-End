f7fabdb6445356e74573c4ee3da0495e4b6e3fbf
NikosSakell <nikossakell02@gmail.com>
2023-05-10 17:48:35 +0300

    added current date
    added current date on patient card

--- app/src/main/java/com/example/physio_plus_app/MainActivity.java
+++ app/src/main/java/com/example/physio_plus_app/MainActivity.java
@@ -3,13 +3,35 @@ package com.example.physio_plus_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView name_tv;
+    TextView age_tv;
+    TextView birthday_tv;
+    TextView address_tv;
+    TextView date_tv;
+
+    String currentDate = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(new Date());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide(); // Hide the action bar
+
+        name_tv = findViewById(R.id.name_tv);
+        age_tv = findViewById(R.id.age_tv);
+        birthday_tv = findViewById(R.id.birthday_tv);
+        address_tv = findViewById(R.id.address_tv);
+        date_tv = findViewById(R.id.date_tv);
+
+        date_tv.setText(currentDate);
+
     }
 }
