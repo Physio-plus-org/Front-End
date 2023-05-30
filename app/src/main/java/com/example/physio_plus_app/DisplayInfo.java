4347324b03539bacf22bc80bf9fb6f3856663da4
NikosSakell <nikossakell02@gmail.com>
2023-05-30 22:20:00 +0300

    created displayinfo class

---
+++ app/src/main/java/com/example/physio_plus_app/DisplayInfo.java
@@ -0,0 +1,90 @@
+package com.example.physio_plus_app;
+
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class DisplayInfo {
+
+    TextView name_tv;
+    TextView age_tv;
+    TextView birthday_tv;
+    TextView address_tv;
+    TextView date_tv;
+
+    String url;
+    OkHttpClient client;
+
+    String currentDate = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(new Date());
+
+    public DisplayInfo(String url, OkHttpClient client, TextView name, TextView age, TextView address, TextView date){
+
+        this.date_tv = date;
+        this.name_tv = name;
+        this.address_tv = address;
+        this.age_tv = age;
+        this.url = url;
+        this.currentDate = currentDate;
+        date_tv.setText(currentDate);
+        this.client = client;
+
+        Request request = new Request.Builder().url(url).build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d("MainActivity","Call failure!"+ e.getMessage());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d("MainActivity","Call Responded!");
+                if (response.isSuccessful()) {
+                    String json = response.body().string();
+                    Gson gson = new Gson();
+                    Log.d("MainActivity", "Server response: " + json);
+
+                    try {
+                        JSONObject jsonObject = new JSONObject(json);
+                        String name = jsonObject.getString("name");
+                        String address = jsonObject.getString("address");
+                        String age = jsonObject.getString(("age"));
+                        // Use the name variable as needed
+                        Log.d("MainActivity", "Name set to: " + name);
+                        Log.d("MainActivity","Address set to:" + address);
+                        Log.d("MainActivity","Age set to:" + age);
+
+                        name_tv.post(()->name_tv.setText(name));
+                        address_tv.post(()->address_tv.setText(address));
+                        age_tv.post(()->age_tv.setText(age));
+                    } catch (JSONException e) {
+                        // Handle JSON parsing error
+                        Log.e("MainActivity", "JSON parsing error", e);
+                    }
+                } else {
+                    //code
+                    Log.e("MainActivity", "Error response: " + response.code());
+                }
+                response.close();
+            }
+
+
+        });
+
+    }
+}
--- app/src/main/java/com/example/physio_plus_app/MainActivity.java
+++ app/src/main/java/com/example/physio_plus_app/MainActivity.java
@@ -8,10 +8,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import com.google.gson.Gson;
 
 import org.json.JSONException;
@@ -21,7 +17,6 @@ import java.io.IOException;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
 import okhttp3.Response;
 
 
@@ -34,10 +29,13 @@ public class MainActivity extends AppCompatActivity {
     TextView birthday_tv;
     TextView address_tv;
     TextView date_tv;
-    public OkHttpClient client;
 
 
-    String currentDate = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(new Date());
+
+    public DisplayInfo handler;
+    String url;
+
+    OkHttpClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,64 +45,15 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d("MainActivity", "onCreate method called");
 
-
         name_tv = findViewById(R.id.name_tv);
         age_tv = findViewById(R.id.age_tv);
-        birthday_tv = findViewById(R.id.yearsold_tv);
         address_tv = findViewById(R.id.address_tv);
         date_tv = findViewById(R.id.date_tv);
 
-        date_tv.setText(currentDate);
+        url = "http://192.168.56.1/displaypatients.php";
 
-        client =  new OkHttpClient();
-
-
-        Request request = new Request.Builder()
-                .url("http://192.168.56.1/patients/displaypatients.php")
-                .build();
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Log.d("MainActivity","Call failure!");
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                Log.d("MainActivity","Call Responded!");
-                if (response.isSuccessful()) {
-                    String json = response.body().string();
-                    Gson gson = new Gson();
-                    Log.d("MainActivity", "Server response: " + json);
-
-                    try {
-                        JSONObject jsonObject = new JSONObject(json);
-                        String name = jsonObject.getString("name");
-                        String address = jsonObject.getString("address");
-                        String age = jsonObject.getString(("age"));
-                        // Use the name variable as needed
-                        Log.d("MainActivity", "Name set to: " + name);
-                        Log.d("MainActivity","Address set to:" + address);
-                        Log.d("MainActivity","Age set to:" + age);
-
-                        runOnUiThread(() -> name_tv.setText(name));
-                        runOnUiThread(()->address_tv.setText(address));
-                        runOnUiThread(()->age_tv.setText(age));
-                    } catch (JSONException e) {
-                        // Handle JSON parsing error
-                        Log.e("MainActivity", "JSON parsing error", e);
-                    }
-                } else {
-                    //code
-                    Log.e("MainActivity", "Error response: " + response.code());
-                }
-                response.close();
-            }
-
-
-        });
-
-
+        OkHttpClient client = new OkHttpClient();
+        handler = new DisplayInfo(url, client ,name_tv, age_tv, address_tv, date_tv);
 
     }
 
