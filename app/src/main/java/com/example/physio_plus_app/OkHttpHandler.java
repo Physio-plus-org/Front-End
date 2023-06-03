922f4e2770a3f70cb9d39c237ab468f17ce47c74
Kostas Thomson <kostasthomson12@gmail.com>
2023-06-03 13:59:00 +0300

    Refactor
    Http Handler refactor:
    - abstract class
    - static method and attributes
    
    Refactor main activity:
    - rename main activity
    - onClick method

--- app/src/main/java/com/example/physio_plus_app/OkHttpHandler.java
+++ app/src/main/java/com/example/physio_plus_app/OkHttpHandler.java
@@ -1,30 +1,24 @@
 package com.example.physio_plus_app;
 
-import android.content.Context;
 import android.os.StrictMode;
-import android.util.Log;
-import android.widget.Toast;
 
 import java.io.IOException;
-import java.util.Iterator;
 
 import okhttp3.FormBody;
-import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
-import okhttp3.Response;
 
-public class OkHttpHandler {
-    public OkHttpHandler() {
-        StrictMode.ThreadPolicy policy = new
-                StrictMode.ThreadPolicy.Builder().permitAll().build();
+public abstract class OkHttpHandler {
+    private static final StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+    public static void insertData(Params params) throws IOException {
         StrictMode.setThreadPolicy(policy);
-    }
+        final String myIP = "192.168.1.6";
+        String url = "http://"+myIP+"/request.php";
 
-    public void insertData(String url, Params params) throws IOException {
         OkHttpClient client = new OkHttpClient().newBuilder().build();
         FormBody.Builder builder = new FormBody.Builder();
+
         if (params != null) {
             builder.add("title", params.getTitle());
             builder.add("desc", params.getDesc());
---
+++ app/src/main/java/com/example/physio_plus_app/R2.java
@@ -0,0 +1,40 @@
+package com.example.physio_plus_app;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+
+
+public class R2 extends AppCompatActivity {
+    EditText name,desc,code,price;
+    Button conf;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        code = findViewById(R.id.codein);
+        name = findViewById(R.id.namein);
+        desc = findViewById(R.id.descin);
+        price = findViewById(R.id.pricein);
+        conf = findViewById(R.id.confirm);
+    }
+
+    public void onClick(View view) {
+        String c,n,d,p;
+        c = code.getText().toString();
+        n = name.getText().toString();
+        d = desc.getText().toString();
+        p = price.getText().toString();
+        try {
+            OkHttpHandler.insertData(new Params(n,d,c,p));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
