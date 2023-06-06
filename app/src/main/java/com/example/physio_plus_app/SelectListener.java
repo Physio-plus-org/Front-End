a053b02cb0cea2cc134f5dd17b8fffbb995ca168
spyridStel <spyridopste@gmail.com>
2023-06-06 16:48:20 +0300

    Revert "basic functionality final"
    This reverts commit 1a039d6db1d01d157d284e498b21725aa70b220c.

--- app/src/main/java/com/example/physio_plus_app/SelectListener.java
+++
@@ -1,6 +0,0 @@
-package com.example.physio_plus_app;
-
-public interface SelectListener {
-    default void onRemoveItemClicked(int eventID) {}
-    default void onCompleteItemClicked(int eventID){}
-}
