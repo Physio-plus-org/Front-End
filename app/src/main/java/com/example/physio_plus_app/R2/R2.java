a8f857041135e794dd6b401c4c6d3dabab55db2b
Kostas Thomson <kostasthomson12@gmail.com>
2023-06-06 22:21:28 +0300

    Rename
    Rename R2 related files

--- app/src/main/java/com/example/physio_plus_app/R2/R2.java
+++ app/src/main/java/com/example/physio_plus_app/R2/R2.java
@@ -18,7 +18,7 @@ public class R2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_r2);
 
         code = findViewById(R.id.codein);
         name = findViewById(R.id.namein);
