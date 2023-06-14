e867ca30e6e0f040d2fa10cc35868b82ba3b558f
Vasilis Tsavalias <70720970+vasilistsavalias@users.noreply.github.com>
2023-06-14 19:41:38 +0300

    added r1-r2

---
+++ app/src/main/java/com/example/physio_plus_app/R2/OkHttpHandlerR2.java
@@ -0,0 +1,35 @@
+package com.example.physio_plus_app.R2;
+
+import android.os.StrictMode;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+public abstract class OkHttpHandlerR2 {
+    private static final StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+    public static void insertData(ParamsR2 params) throws IOException {
+        StrictMode.setThreadPolicy(policy);
+        final String myIP = "192.168.1.6";
+        String url = "http://"+myIP+"/request.php";
+
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        FormBody.Builder builder = new FormBody.Builder();
+
+        if (params != null) {
+            builder.add("title", params.getTitle());
+            builder.add("desc", params.getDesc());
+            builder.add("code", params.getCode());
+            builder.add("cost", params.getPrice());
+        }
+        RequestBody body = builder.build();
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        client.newCall(request).execute();
+    }
+}
