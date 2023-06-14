3fecdbfc5e688270447c565724b788676f401a32
Vasilis Tsavalias <70720970+vasilistsavalias@users.noreply.github.com>
2023-06-14 19:08:15 +0300

    added r5

---
+++ app/src/main/java/com/example/physio_plus_app/R5/Selected_User.java
@@ -0,0 +1,30 @@
+package com.example.physio_plus_app.R5;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.physio_plus_app.R;
+
+public class Selected_User extends AppCompatActivity  {
+
+    Intent intent;
+    User user;
+    TextView infos;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.selected_user_r5);
+        infos = findViewById(R.id.userinfos);
+        intent = getIntent();
+
+        if(intent != null){
+            user = (User) intent.getSerializableExtra("data");
+            String data = user.getFirstName() +" "+ user.getLastName() +" "+user.getAMKA();
+            infos.setText(data);
+        }
+
+    }
+}
