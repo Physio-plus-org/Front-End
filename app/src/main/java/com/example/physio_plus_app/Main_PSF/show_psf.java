172a9a41cbd3e6891e0bef59b271bca48279c1af
Vasilis Tsavalias <v.tsavalias@gmail.com>
2023-06-20 02:01:44 +0300

    added new intent + fixed psf  ui

--- app/src/main/java/com/example/physio_plus_app/Main_PSF/show_psf.java
+++ app/src/main/java/com/example/physio_plus_app/Main_PSF/show_psf.java
@@ -4,36 +4,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.physio_plus_app.R;
 import com.example.physio_plus_app.R1.R1;
 import com.example.physio_plus_app.R2.R2;
 
-public class mainPSF extends AppCompatActivity {
+public class show_psf extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_psf);
+        setContentView(R.layout.psf_show_psfs);
+
 
         //Topbar
         ImageView goBackButton = findViewById(R.id.back_button);
 
         //Footbar
-        ImageButton addButton = findViewById(R.id.add_Button);
-        ImageView physioCenter = findViewById(R.id.physiocenter_btn);
+        ImageView kneadingButton = findViewById(R.id.physio_btn);
+        ImageButton addPSFButton = findViewById(R.id.addPSF_Button);
 
         goBackButton.setOnClickListener(v -> finish());
 
-        addButton.setOnClickListener(v->{
-            Intent i = new Intent(mainPSF.this, R2.class );
+        kneadingButton.setOnClickListener(v -> {
+            Intent i = new Intent(show_psf.this, R1.class);
+            startActivity(i);
+        });
+        addPSFButton.setOnClickListener(v -> {
+            Intent i = new Intent(show_psf.this, R2.class);
             startActivity(i);
         });
 
-
-        physioCenter.setOnClickListener(v->{
-            Intent i = new Intent(mainPSF.this, show_psf.class );
-            startActivity(i);
-        });
     }
-
 }
+
