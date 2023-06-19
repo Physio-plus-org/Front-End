24bc2d556831d876fb1fca9de5aeb5f918b8adc7
Kostas Thomson <kostasthomson12@gmail.com>
2023-06-19 23:28:20 +0300

    Update R5.java

--- app/src/main/java/com/example/physio_plus_app/R5/R5.java
+++ app/src/main/java/com/example/physio_plus_app/R5/R5.java
@@ -1,35 +1,22 @@
 package com.example.physio_plus_app.R5;
 
-import androidx.annotation.NonNull;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.SearchView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.SearchView;
-
-
 import com.example.physio_plus_app.R;
-
 import com.example.physio_plus_app.R3.R3;
-import com.example.physio_plus_app.R6.R6;
-
-import com.example.physio_plus_app.R4.R4;
-import com.example.physio_plus_app.R8.R8;
-
 
 import java.util.ArrayList;
 import java.util.List;
