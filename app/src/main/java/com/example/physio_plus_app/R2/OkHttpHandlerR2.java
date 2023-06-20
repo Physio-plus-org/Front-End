2686aef0cf0eefa4f6fefdade98820b27de46c59
ChrisGiamakidis <87308690+ChrisGiamakidis@users.noreply.github.com>
2023-06-20 20:09:07 +0300

    R2duplicateEntries

--- app/src/main/java/com/example/physio_plus_app/R2/OkHttpHandlerR2.java
+++ app/src/main/java/com/example/physio_plus_app/R2/OkHttpHandlerR2.java
@@ -10,11 +10,12 @@ import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public abstract class OkHttpHandlerR2 {
     private static final String file_folder = "R2/";
     private static final StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-    public static void insertData(ParamsR2 params) throws IOException {
+    public static String insertData(ParamsR2 params) throws IOException {
 //        StrictMode.setThreadPolicy(policy);
 //        final String myIP = "192.168.1.6";
 //        String url = "http://"+myIP+"/request.php";
@@ -30,7 +31,8 @@ public abstract class OkHttpHandlerR2 {
             builder.add("cost", params.getPrice());
         }
         RequestBody body = builder.build();
-        HttpHandler.postRequest(file_folder + file_name, body);
+        Response response = HttpHandler.postRequest(file_folder + file_name, body);
+        return response.body().string();
 //        Request request = new Request.Builder()
 //                .url(url)
 //                .post(body)
