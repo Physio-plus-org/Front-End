ae509306c0ea9b292d63995c472b9c0a20c9ee7e
Kostas Thomson <kostasthomson12@gmail.com>
2023-06-21 23:50:21 +0300

    delete

--- app/src/main/java/com/example/physio_plus_app/R10/ServerResponseExceptionR10.java
+++
@@ -1,7 +0,0 @@
-package com.example.physio_plus_app.R10;
-
-public class ServerResponseExceptionR10 extends Exception {
-    public ServerResponseExceptionR10(String message) {
-        super(message);
-    }
-}
--- app/src/main/java/com/example/physio_plus_app/R8/RegistryHttpHandlerR8.java
+++
@@ -1,9 +0,0 @@
-package com.example.physio_plus_app.R8;
-
-import com.example.physio_plus_app.Utils.RequestParams;
-
-public abstract class RegistryHttpHandlerR8 extends HttpHandlerR8 {
-    static void request(String url, RequestParams params) throws Exception {
-        makeRequest(url, params);
-    }
-}
